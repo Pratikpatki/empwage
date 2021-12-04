d12d9331f42d9a039094f2bf8d587d9d65f9db35
Pratikpatki <pratikpatki7@gmail.com>
2021-12-04 20:50:47 +0530

    add uc-5-wageforamonthuc-5-wageforamonth.sh

---
+++ uc-5-wageforamonth.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+isPartTime=1
+IsFullTime=2
+totalSalary=0
+empRatePerHr=20
+numOfWorkingDays=20
+
+for ((day=1; day<=$numOfWorkingDays; day++ ))
+do
+	empcheck=$((RANDOM%3))
+		case $empcheck in
+				$isPartTime)
+					empHrs=4
+				;;
+
+				$isFullTime)
+					empHrs=8
+				;;
+
+				*)
+					empHrs=0
+				;;
+		esac
+	salary=$(($empHrs*$empRatePerHr))
+	totalSalary=$(($salary+$totalSalary))
+done
